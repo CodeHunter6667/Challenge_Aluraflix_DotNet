cf04f5bb0be5ad04d54b838974fb0b43082b5102
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-03-04 17:40:56 -0300

    feat: Classe de modelo de entidade Videos

---
+++ Aluraflix/Models/Videos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aluraflix.Models;
+
+[Table("tb_videos")]
+public class Videos
+{
+    public long Id { get; set; }
+    public string? Titulo { get; set; }
+    public string? Descricao { get; set; }
+    public string? Url { get; set; }
+
+    public Videos()
+    {
+
+    }
+}
