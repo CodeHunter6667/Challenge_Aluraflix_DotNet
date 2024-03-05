927e14bdfa036ee0fcdc80f92c4486ac4f6a7834
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-03-05 18:54:47 -0300

    feat: Criado DTOs e Mapping para transferencias de dados

---
+++ Aluraflix/DTOs/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AutoMapper;
+
+namespace Aluraflix.DTOs.Mappings
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			CreateMap<VideosDTO, Models.Videos>().ReverseMap();
+		}
+	}
+}
+
---
+++ Aluraflix/DTOs/VideosDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Aluraflix.DTOs
+{
+	public record VideosDTO(
+		long id,
+		[Required(ErrorMessage = "O campo Titulo é obrigatório")]
+		[StringLength(100, MinimumLength = 5, ErrorMessage = "O Titulo deve ter entre 5 e 100 caracteres")]
+		string titulo,
+		[Required(ErrorMessage = "O campo Descricao é obrigatório")]
+		[StringLength(150, MinimumLength = 5, ErrorMessage = "A Descricao deve ter entre 5 e 150 caracteres")]
+		string descricao,
+		[Required(ErrorMessage = "O campo Url é obrigatório")]
+		[StringLength(300, MinimumLength = 5, ErrorMessage = "A Url deve ter entre 5 e 300 caracteres")]
+		string url)
+	{
+		
+	}
+}
+
