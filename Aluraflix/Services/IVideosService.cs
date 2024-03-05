8f2bb0946d2edd45b1e535ead6caaca115dffbfc
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-03-05 18:55:19 -0300

    feat: Criada classe de Servicos para Videos

---
+++ Aluraflix/Services/IVideosService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+using Aluraflix.Models;
+using Aluraflix.Repository;
+
+namespace Aluraflix.Services
+{
+	public interface IVideosService
+	{
+		IQueryable<Videos> GetAll();
+		Task<Videos> GetById(Expression<Func<Videos, bool>> predicate);
+		void Add(Videos entity);
+		void Update(Videos entity);
+		void Delete(Videos entity);
+		Task Commit();
+		IVideosRepository VideosRepository { get; }
+	}
+}
+
---
+++ Aluraflix/Services/VideosService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq.Expressions;
+using Aluraflix.Context;
+using Aluraflix.Models;
+using Aluraflix.Repository;
+using AutoMapper;
+
+namespace Aluraflix.Services
+{
+	public class VideosService : IVideosService
+	{
+        private readonly IMapper _mapper;
+        private VideosRepository _repository;
+        private AppDbContext _context;
+
+        public IVideosRepository VideosRepository
+        {
+            get
+            {
+                return _repository = _repository ?? new VideosRepository(_context);
+            }
+        }
+
+		public VideosService(IMapper mapper, AppDbContext context)
+		{
+            _mapper = mapper;
+            _context = context;
+		}
+
+        public void Add(Videos entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Videos entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Videos> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Videos> GetById(Expression<Func<Videos, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Videos entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
