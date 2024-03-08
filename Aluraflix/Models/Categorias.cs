94d809e109c35c12e4e536078448badf27050861
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-03-08 19:36:19 -0300

    feat: criada entidade Categoria

---
+++ Aluraflix/Models/Categorias.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aluraflix.Models;
+
+[Table("tb_categorias")]
+public class Categorias
+{
+	[Key]
+	public long Id { get; set; }
+	public string? Titulo { get; set; }
+	public string? Cor { get; set; }
+	public List<Videos> Videos { get; set; }
+
+	public Categorias()
+	{
+		Videos = new List<Videos>();
+	}
+}
+
