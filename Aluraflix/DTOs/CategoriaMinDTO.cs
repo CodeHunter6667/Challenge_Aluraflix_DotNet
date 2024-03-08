96fde3dcd33baf018ab93e729a9b3acc602bd10e
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-03-08 19:54:06 -0300

    configuracoes do programa

--- Aluraflix/DTOs/CategoriaMinDTO.cs
+++ Aluraflix/DTOs/CategoriaMinDTO.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Aluraflix.DTOs
 {
-	public class CategoriaMinDTO
+	public record CategoriaMinDTO(
+		long id,
+		[Required(ErrorMessage = "O campo Titulo é obrigatório")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage ="Titulo deve ter entre 5 e 100 caracteres")]
+		string? titulo,
+        [Required(ErrorMessage = "O campo Cor é obrigatório")]
+		[StringLength(15, MinimumLength =5, ErrorMessage ="Cor deve ter entre 5 e 15 caracteres")]
+        string? cor)
 	{
-		public CategoriaMinDTO()
-		{
-		}
+		
 	}
 }
 
