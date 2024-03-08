e3cbc45f1d2057d50b0d0831561b3ab7488298b7
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-03-08 20:51:18 -0300

    fix: alteracoes de retorno

--- Aluraflix/Services/ICategoriasService.cs
+++ Aluraflix/Services/ICategoriasService.cs
@@ -5,10 +5,10 @@ namespace Aluraflix.Services
 {
 	public interface ICategoriasService
 	{
-		List<CategoriasDTO> GetAll(int skip, int take);
+		List<CategoriasMinDTO> GetAll(int skip, int take);
 		CategoriasDTO GetBYId(long id);
-	    CategoriasDTO Add(CategoriasDTO entity);
-		CategoriasDTO Update(long id, CategoriasDTO dto);
+	    CategoriasMinDTO Add(CategoriasMinDTO entity);
+		CategoriasMinDTO Update(long id, CategoriasMinDTO dto);
 		void Delete(long id);
 		void Commit();
 	}
